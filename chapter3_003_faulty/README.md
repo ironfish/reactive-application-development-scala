2d1de473a2322cbb7fb50727c1c4bdcc70649a88
duncan.devore@gmail.com <duncan.devore@gmail.com>
2015-04-26 17:50:13 -0400

    Fixes #4 faulty router

---
+++ chapter3_003_faulty/README.md
@@ -0,0 +1,22 @@
+# Chapter 3: Akka Basic Toolkit - Faulty Router
+
+In this source code example we will introduce failure to simulate when a librarian receives too many complaints. Our basic actor layout will stay the same with a small change to our `Librarian`:
+
+## `Librarian` Actor
+
+1. Implement a `max-complaint-count` for our `Librarian`.
+2. When the number of `Complain` messages received equals the `max-complain-count`, out `Librarian` will throw a `ComplainException`.
+
+## Summary
+
+We have introduced failure to our scenario. When a `Customer` complains, our `Librarian` will eventually become frustrated and throw an exception. In turn, this means our `Customer` will not receive a `Credit` and will stop requesting information. Given enough time, all customer's will eventually stop requesting information essentially grinding our application to a stand still. We will address this problem in the next section with `self-healing` resilience.
+
+## Running the Application
+
+1. Run a terminal session and navigate to the root directory, `reactive-application-development-scala`.
+2. Run `sbt`.
+3. Inside the `sbt` session, enter `project chapter3_003_faulty`.
+4. Inside the `sbt` session, enter `run` to bootstrap the application.
+5. Under `Enter commands [q = quit, 2c = 2 customers, etc.]:` enter `5c` for 5 customers.
+6. The `rarebook.log` file will be created in the root directory containing the application output.
+7. You will notice that while we still have a **significant increase in throughput**, the introduced failure will eventually cause all customers to stop requesting book information.
---
+++ chapter3_003_faulty/src/test/scala/com/rarebooks/library/CatalogSpec.scala
@@ -0,0 +1,79 @@
+package com.rarebooks.library
+
+class CatalogSpec extends BaseSpec {
+
+  import Catalog._
+  import RareBooksProtocol._
+
+  "books" should {
+    "contain theEpicOfGilgamesh, phaedrus and theHistories" in {
+      books.values.toSet should === (Set[BookCard](theEpicOfGilgamesh, phaedrus, theHistories))
+    }
+  }
+
+  "findBookByIsbn" should {
+    "return theEpicOfGilgamesh" in {
+      findBookByIsbn(theEpicOfGilgamesh.isbn) should === (Some[List[BookCard]](List(theEpicOfGilgamesh)))
+    }
+    "return phaedrus" in {
+      findBookByIsbn(phaedrus.isbn) should === (Some[List[BookCard]](List(phaedrus)))
+    }
+    "return theHistories" in {
+      findBookByIsbn(theHistories.isbn) should === (Some[List[BookCard]](List(theHistories)))
+    }
+    "return None when isbn not found" in {
+      findBookByIsbn("1234567890") should === (None)
+    }
+  }
+
+  "findBookByAuthor" should {
+    "return theEpicOfGilgamesh" in {
+      findBookByAuthor(theEpicOfGilgamesh.author) should === (Some[List[BookCard]](List(theEpicOfGilgamesh)))
+    }
+    "return phaedrus" in {
+      findBookByAuthor(phaedrus.author) should === (Some[List[BookCard]](List(phaedrus)))
+    }
+    "return theHistories" in {
+      findBookByAuthor(theHistories.author) should === (Some[List[BookCard]](List(theHistories)))
+    }
+    "return None when author not found" in {
+      findBookByAuthor("Michael Crichton") should === (None)
+    }
+  }
+
+  "findBookByTitle" should {
+    "return theEpicOfGilgamesh" in {
+      findBookByTitle(theEpicOfGilgamesh.title) should === (Some[List[BookCard]](List(theEpicOfGilgamesh)))
+    }
+    "return phaedrus" in {
+      findBookByTitle(phaedrus.title) should === (Some[List[BookCard]](List(phaedrus)))
+    }
+    "return theHistories" in {
+      findBookByTitle(theHistories.title) should === (Some[List[BookCard]](List(theHistories)))
+    }
+    "return None when title not found" in {
+      findBookByTitle("Swiss Family Robinson") should === (None)
+    }
+  }
+
+  "findBookByTopic" should {
+    "for Gilgamesh, Persia and Royalty, return theEpicOfGilgamesh" in {
+      findBookByTopic(Set(Gilgamesh, Persia, Royalty)) should === (Some[List[BookCard]](List(theEpicOfGilgamesh)))
+    }
+    "for Philosophy, return phaedrus" in {
+      findBookByTopic(Set(Philosophy)) should === (Some[List[BookCard]](List(phaedrus)))
+    }
+    "for Greece and Philosophy, return phaedrus and theHistories" in {
+      findBookByTopic(Set(Greece, Philosophy)) should === (Some[List[BookCard]](List(phaedrus, theHistories)))
+    }
+    "for Africa, Asia, Greece and Tradition, return phaedrus and theHistories" in {
+      findBookByTopic(Set(Africa, Asia, Greece, Tradition)) should === (Some[List[BookCard]](List(phaedrus, theHistories)))
+    }
+    "for Tradition, return theHistories" in {
+      findBookByTopic(Set(Tradition)) should === (Some[List[BookCard]](List(theHistories)))
+    }
+    "return None when tag not found" in {
+      findBookByTopic(Set(Unknown)) should === (None)
+    }
+  }
+}
